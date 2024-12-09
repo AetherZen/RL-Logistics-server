f3edf88045e5385888867224e6188a069321d5e4
Rakib Hasan <rakibnkhadija@gmail.com>
2024-12-09 19:12:32 +0600

    refresh some code

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,11 @@
 const express =require("express");
-
 const router = express.Router();
-
 // middlewares
 const {authenticated,admin}= require("../middleware/authentication.js") ;
 const {testUser}= require("../middleware/testUser.js") ;
-// controllers
+
+
+// *************controllers************
 const {
   register,
   login,
@@ -17,20 +17,17 @@ const {
   updateRole,
 } =require("../controllers/auth.js");
 
+
+// ***********navigate way***************
 router.post("/register", register);
 router.post("/login", login);
 router.get("/login-check", authenticated, isLoginCheck);
 router.get("/admin-check", authenticated, admin,isAdminCheck);
-
-//logged in but if test user then can't update profile
-router.put("/profile", authenticated,testUser, updateProfile);
-
-// testing
-router.get("/secret", authenticated, admin, secret);
+router.put("/profile", authenticated,testUser, updateProfile); //logged in but if test user then can't update profile
+router.get("/secret", authenticated, admin, secret); // testing
+router.get("/all-users", authenticated, admin, getAllUsers);   // get all users
+router.put("/admin/update-role", authenticated, admin, updateRole);  //change user role
 
 
-// get all users
-router.get("/all-users", authenticated, admin, getAllUsers);
-//change user role
-router.put("/admin/update-role", authenticated, admin, updateRole);
+// ***********export default****************
 module.exports= router;
